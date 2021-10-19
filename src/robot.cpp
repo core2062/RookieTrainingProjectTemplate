40974bc03377f3ecd49aeb5e72dc67888e9c8fe9
CORE Programmers <core2062programmers@googlegroups.com>
2021-10-19 17:29:49 -0500

    inital commit

---
+++ src/robot.cpp
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
+#include <iostream>
+//Vars
+/*bool m_AButton = false;
+frc::DoubleSolenoid::Value m_desiredGear = frc::DoubleSolenoid::Value::kForward;*/
+
+
+Robot::Robot(): {}
+{}
+
+void Robot::robotInit() {
+  
+}
+
+void Robot::teleopInit() {}
+
+void Robot::teleop() {
+  
+  }
+}
+
+void Robot::testInit() {}
+
+void Robot::test() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
---
+++ src/robot.h
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <string>
+
+#include <ctre/Phoenix.h>
+#include <CORERobotLib.h>
+#include <COREFramework/COREScheduler.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/Compressor.h>
+using namespace frc;
+
+class Robot : public CORERobot {
+ public:
+  Robot();
+  void robotInit() override;
+  void teleop() override;
+  void teleopInit() override;
+  void test() override;
+  void testInit() override;
+
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  
+};
+
+//hello
